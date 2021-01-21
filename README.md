ba86053c5cc99fe3190033634c2ef983d130c133
farizulhammi <fariznyafariz@gmail.com>
2021-01-21 21:32:09 +0700

    w

---
+++ README.md
@@ -0,0 +1,1 @@
+# topplaylist-data
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include 'dom.php';
+$html = file_get_html('https://musiclist.com/en/top-ventas-indonesia');
+
+foreach ($html->find('h4[class=truncate text-sm font-bold text-gray-600]') as $key) {
+	$data = $key->plaintext;
+	$dataa[] =$data;
+}
+print_r($dataa);
+// print_r(json_encode($dataa));
+// print_r(json_decode($dataw));
